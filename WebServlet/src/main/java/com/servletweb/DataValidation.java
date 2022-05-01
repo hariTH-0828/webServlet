4974f9a0320f9dadbf7dc139cd8935ff87ef71c4
Hariharanrs_0828 <hariharan08112001@gmail.com>
2022-05-01 11:41:04 +0530

    update

--- WebServlet/src/main/java/com/servletweb/DataValidation.java
+++ WebServlet/src/main/java/com/servletweb/DataValidation.java
@@ -9,23 +9,29 @@ import javax.xml.crypto.Data;
 public class DataValidation {
 
 	public static boolean validate(String userid, String password) {
-		String uid = null, pass = null, res = null;
+		String uid = null, pass = null;
+		boolean res = false;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root","root");
 			uid = userid;
 			pass = password;
-			String query = "select * from studentlogin where ID=?";
+			String query = "select * from studentlogin where ID = ? AND BINARY Password = ?";
 			PreparedStatement st = con.prepareStatement(query);
 			st.setString(1, uid);
+			st.setString(2, pass);
 			ResultSet rs = st.executeQuery();
-			rs.next();
-			res = rs.getString("password");
+			res = rs.next();
+			//res = rs.getString("password");
 		}catch (Exception e) {
 			System.out.println(e);
 		}
-		if(pass.equals(res)) {
+		if(res) {					//pass.equals(res)
 			return true;
 		}else return false;
 	}
+	/*
+	 * public static void main(String args[]) {
+	 * System.out.println(DataValidation.validate("1902001", "Guru@2001")); }
+	 */
 }
